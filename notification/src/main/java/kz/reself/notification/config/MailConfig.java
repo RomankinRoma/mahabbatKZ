eb422016c5ab844cf73b7c65537525ce81818f9f
Adil <adil.kenesbek21@gmail.com>
2021-09-26 18:44:12 +0600

    struct

---
+++ notification/src/main/java/kz/reself/notification/config/MailConfig.java
@@ -0,0 +1,31 @@
+package kz.reself.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("kz.reself.notification")
+public class MailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("romankin.roma.rakho@gmail.com");
+        mailSender.setPassword("Romankin345678");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
