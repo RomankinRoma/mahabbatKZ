c6b7cef52bc0ca80562a8f9be0cf6d58f9672f44
Adil <adil.kenesbek21@gmail.com>
2021-10-03 14:49:32 +0600

    eureka

--- business/src/main/java/kz/reself/business/BusinessApplication.java
+++ business/src/main/java/kz/reself/business/BusinessApplication.java
@@ -2,10 +2,12 @@ package kz.reself.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "kz.reself")
+@EnableDiscoveryClient
 public class BusinessApplication {
 
     public static void main(String[] args) {
