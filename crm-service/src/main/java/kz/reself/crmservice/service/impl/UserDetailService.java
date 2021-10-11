4cc8091db48345d594f2b85e5142236f36397761
nzansv <63594033+nzansv@users.noreply.github.com>
2021-10-12 04:55:52 +0600

    evrica

---
+++ crm-service/src/main/java/kz/reself/crmservice/service/impl/UserDetailService.java
@@ -0,0 +1,26 @@
+package kz.reself.crmservice.service.impl;
+
+import kz.reself.crmservice.repository.InterestRepository;
+import kz.reself.crmservice.repository.UserDetailRepository;
+import kz.reself.crmservice.service.IUserDetailService;
+import kz.reself.dbstruct.model.UsersDetail;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailService implements IUserDetailService {
+
+    @Autowired
+    private UserDetailRepository usersDetailRepository;
+
+    @Autowired
+    private InterestRepository interestRepository;
+
+    @Override
+    public UsersDetail getById(Long userId) {
+        return usersDetailRepository.getById(userId);
+    }
+
+}
