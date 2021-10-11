78fc97e190789f91f3337b486cd40443e0beb52b
nzansv <63594033+nzansv@users.noreply.github.com>
2021-10-11 18:22:10 +0600

    evrica

--- crm-service/src/main/java/kz/reself/crmservice/CrmServiceApplication.java
+++ crm-service/src/main/java/kz/reself/crmservice/CrmServiceApplication.java
@@ -2,8 +2,10 @@ package kz.reself.crmservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "kz.reself")
+@EnableEurekaClient
 public class CrmServiceApplication {
 
     public static void main(String[] args) {
--- db-struct/src/main/java/kz/reself/dbstruct/DbStructApplication.java
+++ db-struct/src/main/java/kz/reself/dbstruct/DbStructApplication.java
@@ -2,8 +2,10 @@ package kz.reself.dbstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "kz.reself")
+@EnableEurekaClient
 public class DbStructApplication {
 
     public static void main(String[] args) {
