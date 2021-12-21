6c2e93f3fdfce7f81cd873dbd1adea974e19b47b
Romankin <raxo00012345@gmail.com>
2021-12-21 12:17:12 +0600

    storage service

---
+++ storage/src/test/java/kz/reself/storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.reself.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
